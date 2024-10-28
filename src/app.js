f7706699996e79c3ba782840301a0b603b51db89
RyanSolutech <158137177+RyanSolutech@users.noreply.github.com>
2024-10-28 23:01:47 +0300

    working changes

--- src/app.js
+++ src/app.js
@@ -59,7 +59,7 @@ app.post("/api/transactions", async (req, res) => {
 // Get all transactions
 app.get("/api/transactions", async (req, res) => {
   try {
-    const transactions = await Transaction.findAll();
+    const transactions = await Transaction.findAll({ order: [["createdAt", "DESC"]] });
     res.json(transactions);
   } catch (error) {
     res.status(500).json({ error: error.message });
